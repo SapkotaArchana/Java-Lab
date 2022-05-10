65f1394c77babce2a8e966c93e90485a313a1443
Archana Sapkota <sapkotaarchana6@gmail.com>
2022-05-10 12:42:07 +0545

    Modified

---
+++ Lab/fibonacci123.java
@@ -0,0 +1,30 @@
+package Lab;
+import java.util.Scanner;
+class fibo{
+    int fib(int n){
+        if(n==0){
+            return 0;
+        }
+        else if(n==1){
+            return 1;
+        }
+        else{
+            return (fib(n-2)+fib(n-1));
+        }
+    }
+}
+public class fibonacci123 {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        fibo f=new fibo();
+        System.out.println("Enter number");
+        int n=sc.nextInt();
+        System.out.println("The list of fibonacci series are:");
+        for(int i=0;i<n;i++){
+            System.out.print(f.fib(i)+" ");
+        }
+        System.out.println("\n"+"The fourth element of fibonacci series is:"+f.fib(4));
+        
+    }
+    }
+
